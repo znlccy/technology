bf155daeb1c0e936ffeb90e731fbf3b9af78fc72
znlccy <756311868@qq.com>
2018-11-13 17:30:29 +0800

    添加订单和物流验证器

--- application/admin/validate/Logistics.php
+++ application/admin/validate/Logistics.php
@@ -13,16 +13,27 @@ class Logistics extends BasisValidate {
 
     /* 验证规则 */
     protected $rule = [
-
+        'id'        => 'number',
+        'name'      => 'max:255',
+        'code'      => 'number',
+        'page_size' => 'number',
+        'jump_page' => 'number'
     ];
 
     /* 验证字段 */
     protected $field = [
-
+        'id'        => '物流主键',
+        'name'      => '物流名称',
+        'code'      => '物流代码',
+        'page_size' => '分页大小',
+        'jump_page' => '跳转页'
     ];
 
     /* 验证场景 */
     protected $scene = [
-
+        'listing'       => ['id' => 'number', 'name' => 'max:255', 'code' => 'number', 'page_size' => 'number', 'jump_page' => 'number'],
+        'save'          => ['id' => 'number', 'name' => 'require|max:255', 'code' => 'require|number'],
+        'detail'        => ['id' => 'require|number'],
+        'delete'        => ['id' => 'require|number'],
     ];
 }
--- application/admin/validate/Order.php
+++ application/admin/validate/Order.php
@@ -13,17 +13,48 @@ class Order extends BasisValidate {
 
     /* 验证规则 */
     protected $rule = [
-
+        'id'            => 'number',
+        'order_id'      => 'max:255',
+        'amount_start'  => 'float',
+        'amount_end'    => 'float',
+        'pay_method'    => 'number',
+        'status'        => 'number',
+        'subject'       => 'max:300',
+        'body'          => 'max:500',
+        'create_start'  => 'date',
+        'create_end'    => 'date',
+        'update_start'  => 'date',
+        'update_end'    => 'date',
+        'page_size'     => 'number',
+        'amount'        => 'float',
+        'jump_page'     => 'number'
     ];
 
     /* 验证消息 */
     protected $field = [
-
+        'id'            => '订单自增主键',
+        'order_id'      => '订单主键',
+        'amount_start'  => '订单金额起始区间',
+        'amount_end'    => '订单金额截止区间',
+        'pay_method'    => '订单支付方式',
+        'status'        => '订单状态',
+        'subject'       => '订单主题',
+        'body'          => '订单描述',
+        'create_start'  => '订单创建起始时间',
+        'create_end'    => '订单创建截止时间',
+        'update_start'  => '订单更新起始时间',
+        'update_end'    => '订单更新截止时间',
+        'page_size'     => '分页大小',
+        'jump_page'     => '跳转页'
     ];
 
     /* 验证场景 */
     protected $scene = [
-
+        'listing'       => ['id' => 'number', 'order_id' => 'max:255', 'amount_start' => 'float', 'amount_end' => 'float', 'pay_method' => 'number', 'status' => 'number', 'subject' => 'max:255', 'body' => 'max:500', 'create_start' => 'date', 'create_end' => 'date', 'update_start' => 'date', 'update_end' => 'date', 'page_size' => 'number', 'jump_page' => 'number'],
+        'save'          => ['id' => 'number', 'amount' => 'require|float', 'pay_method' => 'number', 'status' => 'require|number|in:0,1', 'subject' => 'require|max:255', 'body' => 'require|max:500'],
+        'detail'        => ['id' => 'require|number'],
+        'delete'        => ['id' => 'require|number'],
+        'supplement'    => ['order_id' => 'require|max:255']
     ];
 
 }
